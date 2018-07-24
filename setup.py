dae19e9a06f560f9ba6332796eb44557a355fe2b
Gabriel Reis <gabriel.reis@gazprom-mt.com>
2018-07-24 11:07:40 +0100

    Injecting builtin using pth file

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 import io
+import os
 
 from setuptools import setup
+from setuptools.command.install import install
 
+PTH_FILE = 'python_ls.pth'
 
 with io.open('README.md', encoding='UTF-8') as fp:
     long_description = fp.read()
+
+
+class install_with_pth(install):
+    @property
+    def target(self):
+        return os.path.join(self.install_lib, PTH_FILE)
+
+    def get_outputs(self):
+        outputs = install.get_outputs(self) or []
+        return outputs + [self.target]
+
+    def run(self):
+        ret = install.run(self)
+        with open(PTH_FILE) as ifp:
+            with open(self.target, 'w') as ofp:
+                ofp.write(ifp.read())
+        return ret
 
 
 setup(
@@ -12,7 +32,7 @@ setup(
     use_scm_version={'write_to': 'python_ls/_version.py'},
     setup_requires=['setuptools_scm'],
     packages=['python_ls'],
-    data_files = [("", ["LICENSE"])],
+    data_files=[("", ["LICENSE"])],
     author='Gabriel Reis',
     author_email='gabrielcnr@gmail.com',
     description='A better replacement for Python dir() builtin function',
@@ -33,4 +53,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    cmdclass={
+        'install': install_with_pth,
+    }
 )
