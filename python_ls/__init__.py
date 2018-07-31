0bd00087b478cc7520f61366dbbe734e99ec5fb4
Gabriel Reis <gabrielcnr@gmail.com>
2018-07-31 13:55:30 +0100

    setting up project for CI

--- python_ls/__init__.py
+++ python_ls/__init__.py
@@ -1,1 +1,9 @@
 from ._ls import iter_ls, ls, xdir
+
+try:
+    import __builtin__ as builtins # python 2.7
+except:
+    import builtins # python 3
+
+# injecting the name ls in the builtin namespace
+builtins.ls = ls
